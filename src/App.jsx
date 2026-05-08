50ec485c5524b07bcdec32aace967e798f02eb44
Wagner Pereira Rosario Junior <pvg12207@mg.senac.br>
2026-05-08 10:39:15 -0300

    fix rota raiz definitiva

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
-﻿import { Routes, Route, Navigate } from "react-router-dom";
+﻿import { Routes, Route } from "react-router-dom";
 import Admin from "./Admin";
 import Cliente from "./Cliente";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Navigate to="/cliente" />} />
       <Route path="/" element={<Cliente />} />
       <Route path="/admin" element={<Admin />} />
     </Routes>
