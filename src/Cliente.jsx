ccf9509dcb4cdac8fe3ffd88d1c6539b1a415514
Wagner Junior <owagnerjr@gmail.com>
2026-05-08 21:23:46 -0300

    cliente final corrigido bloqueio domingo e segunda

--- src/Cliente.jsx
+++ src/Cliente.jsx
@@ -232,7 +232,7 @@ function Cliente() {
               
              onClick={() => setTela("home")}
               style={{
-                marginTop: "100px",
+                marginTop: "70px",
                 padding: "10px 20px",
                 background: "transparent",
                 color: "#d4af37",
